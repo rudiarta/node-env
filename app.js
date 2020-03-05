030b70fb9fba15b04a0ab392f263318ddd914c7b
rudirahardian <rudi.rahardian@ralali.com>
2020-03-05 15:22:09 +0700

    modify structure

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const routes = require('./app/route/routesV1');
+const routesV1 = require('./app/routes/routesV1');
 const app = express();
 
 //Routes initialize
-app.use('/v1', routes);
+app.use('/v1', routesV1);
 
 //App start listen on port 3000
 app.listen('3000');
