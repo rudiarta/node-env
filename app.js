e53a47135e8119cdb28810df3b71a52175214926
rudirahardian <rudi.rahardian@ralali.com>
2020-03-05 16:06:50 +0700

    modify structure

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routesV1 = require('./app/routes/routesV1');
 const app = express();
+require('dotenv').config();
 
 //Routes initialize
 app.use('/v1', routesV1);
 
 //App start listen on port 3000
-app.listen('3000');
+app.listen(process.env.PORT);
